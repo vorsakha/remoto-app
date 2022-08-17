e77a1c483e3f3d6a9e9b63f805d7dd43bf21cb5e
vorsakha <andreitf.dev@gmail.com>
2022-08-17 18:51:35 -0300

    refactor: modify the container component so that it can be a scrollview as well as a regular view

--- src/components/common/Container/Container.tsx
+++ src/components/common/Container/Container.tsx
@@ -1,8 +1,20 @@
-import { PropsWithChildren } from 'react';
-import { ContainerWrapper } from './styles';
+import { ScrollViewProps, ViewProps } from 'react-native';
+import { ContainerScrollView, ContainerView } from './styles';
 
-function Container({ children, ...rest }: PropsWithChildren) {
-  return <ContainerWrapper {...rest}>{children}</ContainerWrapper>;
+type ContainerProps = {
+  scroll?: boolean;
+} & (ViewProps | ScrollViewProps);
+
+function Container({ children, scroll, ...rest }: ContainerProps) {
+  return scroll ? (
+    <ContainerScrollView {...rest}>{children}</ContainerScrollView>
+  ) : (
+    <ContainerView {...rest}>{children}</ContainerView>
+  );
 }
+
+Container.defaultProps = {
+  scroll: false,
+};
 
 export default Container;
--- src/components/common/Container/styles.ts
+++ src/components/common/Container/styles.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native';
 
-export const ContainerWrapper = styled.View`
+export const ContainerView = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.gray._100};
+`;
+
+export const ContainerScrollView = styled.ScrollView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.gray._100};
+  height: 100%;
 `;
